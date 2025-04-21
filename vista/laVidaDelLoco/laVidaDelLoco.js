d7b46134640068fb11eef4b083d3b8dce07172ea
Moiselin69 <moisesramirezserrador@gmail.com>
2025-04-21 23:16:50 +0200

    La vide del loco arreglado

--- vista/laVidaDelLoco/laVidaDelLoco.js
+++ vista/laVidaDelLoco/laVidaDelLoco.js
@@ -1,8 +1,14 @@
-window.onload = function() { 
+window.onload = function() {
+    // Seleccionar todos los spans dentro de la cabecera
+    const spans = document.querySelectorAll('.cabecera span');
+    
+    // Array de colores para el efecto
+    const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff'];
+    
     setInterval(function() {
-    spans.forEach(span => {
-        const randomColor = colors[Math.floor(Math.random() * colors.length)]; // Selecciona un color aleatorio
-        span.style.color = randomColor; // Cambia el color de la letra
-    });
-}, 500)
-}
+        spans.forEach(span => {
+            const randomColor = colors[Math.floor(Math.random() * colors.length)];
+            span.style.color = randomColor;
+        });
+    }, 500);
+};
